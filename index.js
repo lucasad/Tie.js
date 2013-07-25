3dd628cafc77edb5522a78242d8ef8b54f62b392
Lucas Dohring <lucas@xn--80atc0ah.com>
2013-07-25 09:46:04 -0700

    Works in browser

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-var map = function(root, context) {
-    for(key in context) {
-        var node = root.find('.'+key);
-        var data = context[key];
-        tie(node, data)
-    }
-}
+/*
+ * Copyright 2013 Lucas A. Dohring <lucas@dohring.tk>
+ * Licensed under the EUPL V.1.1
+ */
+
+(function () {
+    "use strict";
+    var map = function (root, context) {
+        for (var key in context) {
+            var node = root.find('.' + key);
+            var data = context[key];
+            tie(node, data);
+        }
+    };
 
 
-var tie = function(node, data) {
-    switch(data.constructor) {
-    case Array:
-        var inner = node.children();
-        node.empty();
-        data.forEach(function(item) {
-            instance = inner.clone();
-            glue(instance, item)
-            node.append(instance)
-        });
-        break;
-    case Object:
-        map(node, data)
-        break;
-    case Function:
-        data = data()
-    default:
-        data = data.toString()
-    case String:
-        node.html(data)
-        break;
-    }
-}
-
-module.exports = tie
+    var tie = function (node, data) {
+        switch (data.constructor) {
+            case Array:
+                var inner = node.children();
+                node.empty();
+                data.forEach(function (item) {
+                    var instance = inner.clone();
+                    tie(instance, item);
+                    node.append(instance);
+                });
+                inner.remove();
+                break;
+            case Object:
+                map(node, data);
+                break;
+            case Function:
+                node.html(data());
+                break;
+            default:
+                node.html(data.toString());
+                break;
+            case String:
+                node.html(data);
+                break;
+        }
+    };
+    ((typeof module === "undefined") ? window : module.exports).tie=tie;
+})();
